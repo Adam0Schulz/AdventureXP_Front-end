a6c8ee3f02efa52702ce08c481f194b6a759e855
Dániel Szabó <kingalistairlol@gmail.com>
2022-09-28 13:13:08 +0200

    Added url id reader

--- JS/Service/fetchActivityDescriptions.js
+++ JS/Service/fetchActivityDescriptions.js
@@ -2,22 +2,22 @@ const apiUrl = "http://localhost:8080/activities"
 const activityName = document.querySelector(".activityTitle");
 const activityDescription = document.querySelector(".descriptionText");
 
-const id = 1;
+//Get id from URL sent by acitivities.html
+const param = new URLSearchParams(window.location.search);
+const urlId = param.get("id");
+const id = urlId - 1;
 
 getIt();
 
-
 function out(any){
-    console.log(any)
+    console.log(any);
 }
 
 async function getIt(){
-
    out("inside getIt()")
-    const response = await fetch(apiUrl)
+    const response = await fetch(apiUrl);
     const data = await response.json();
-    console.log(data)
-    activityName.textContent = data[id].name
-    activityDescription.textContent = data[id].description
-
+    console.log(data);
+    activityName.textContent = data[id].name;
+    activityDescription.textContent = data[id].description;
 }
