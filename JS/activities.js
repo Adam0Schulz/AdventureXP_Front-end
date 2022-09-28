791e65c9be6cae3e7298cdf80d4770a1985c85c0
Adam0Schulz <adam@schulz.sk>
2022-09-28 14:31:56 +0200

    added js functionality to activities page

--- JS/activities.js
+++ JS/activities.js
@@ -1,1 +1,27 @@
+let response = []
+const activitiesCont = document.querySelector('#activities_cont')
+
+async function fetchAll() {
+    return await fetch("http://localhost:8080/activities")
+        .then(response => response.json())
+}
+
+fetchAll().then(r => {
+    r.forEach(item => {
+        let a = document.createElement("a")
+        a.href = "activityDescription.html?id=" + item.id
+        let div = document.createElement("div")
+        div.className = "activity"
+        div.innerText = item.name
+        a.appendChild(div)
+        activitiesCont.appendChild(a)
+    })
+})
+
+
+
+
+
+
+
 
