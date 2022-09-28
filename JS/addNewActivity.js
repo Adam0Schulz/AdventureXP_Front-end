c274ce951fcf25b9e20ca38611e8ba37ae2e6334
Arkitekt-s <sfattahieh@gmail.com>
2022-09-28 12:21:49 +0200

    js file

--- JS/addNewActivity.js
+++ JS/addNewActivity.js
@@ -1,16 +1,38 @@
 //add save button with event listener
 const saveButton = document.getElementById(".saveButton");
-const cancleButton = document.getElementById(".cancelButton");
-saveButton.addEventListener("click", saveActivity);
-cancleButton.addEventListener("click", cancelActivity);
-//make a save activity function
-function saveActivity() {
-    //get the values from the form
-    const name = document.getElementById(".title").value;
-    const type = document.getElementById(".description").value;
+const url= "http://localhost:9090/activity";
+const name = document.getElementById(".title").value;
+const type = document.getElementById(".description").value;
 
+
+//fetch url
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        //
+
+
+
+    })
+    .catch(error => console.log(error))
+async function getAllData() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const table = data.data;
+
+
+        table.forEach();
+
+        console.log(table);
+
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
-function cancelActivity() {
 
+const DataInfo=getAllData();
+console.log(DataInfo)
 
-}
