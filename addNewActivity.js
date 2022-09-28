24df0a6eb91c35505665d2eed6f6231266cf3f00
Arkitekt-s <sfattahieh@gmail.com>
2022-09-28 09:28:30 +0200

    js file

---
+++ addNewActivity.js
@@ -0,0 +1,4 @@
+//add save button with event listener
+const saveButton = document.getElementById(".saveButton");
+const cancleButton = document.getElementById(".cancleButton");
+saveButton.addEventListener("click", saveActivity);
