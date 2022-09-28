f070caed5af8e3fe4ae8acf235814eb74a25937b
Emie0038 <Emie0038>
2022-09-28 11:52:43 +0200

    small

--- JS/addNewActivity.js
+++ JS/addNewActivity.js
@@ -1,8 +1,9 @@
 //add save button with event listener
 const saveButton = document.getElementById(".saveButton");
-const cancleButton = document.getElementById(".cancelButton");
+const cancelButton = document.getElementById(".cancelButton");
+
 saveButton.addEventListener("click", saveActivity);
-cancleButton.addEventListener("click", cancelActivity);
+cancelButton.addEventListener("click", cancelActivity);
 //make a save activity function
 function saveActivity() {
     //get the values from the form
