343dc51421bd36c2b628c9b7ebb0afe15d322ad2
Arkitekt-s <sfattahieh@gmail.com>
2022-10-05 12:58:53 +0200

    customersPage

--- JS/customerPage.js
+++ JS/customerPage.js
@@ -12,7 +12,7 @@ const keyword = urlKeyword;*/
 
 
 
-import { getAll  } from "./Service/API_calls.js";
+import { getAll, create  } from "./Service/API_calls.js";
 
 const customersCont = document.querySelector('#customers_cont')
 
@@ -22,12 +22,31 @@ getAll("customers").then(r => {
         let a = document.createElement("a")
         let div = document.createElement("div")
         div.className = "customer"
-        div.innerText = item.firstname + " " + item.lastname
+        //make it in table
+        div.innerHTML = "<table><tr><td>ID: </td><td>" + item.id + "</td></tr><tr><td>Name: </td><td>" + item.firstname + "</td></tr><tr><td>lastname: </td><td>" + item.lastname + "</td></tr><tr><td>Phone: </td><td>" + item.phone + "</td></tr><tr><td>Email: </td><td>" + item.email + "</td></tr></table>"
+
+
 
 
         a.appendChild(div)
         customersCont.appendChild(a)
     })
+
+    //creat new customer
+    const createCustomer = document.querySelector('#addBtn')
+    createCustomer.addEventListener('click', () => {
+        let newCustomer = {
+            firstname: document.querySelector('#firstname').value,
+            lastname: document.querySelector('#lastname').value,
+            phone: document.querySelector('#phone').value,
+            email: document.querySelector('#email').value,
+        }
+        create(newCustomer, "customers").then(r => {
+            console.log(r)
+            window.location.href = "customers.html"
+        })
+    }
+    )
 })
 
 
