b40693658854ece5f52880ed7fd65ffd0691d9b6
nika7724 <nika0147@stud.kea.dk>
2022-10-06 13:03:35 +0200

    dropdown

--- JS/createBooking.js
+++ JS/createBooking.js
@@ -2,4 +2,15 @@ import { getAll, create  } from "./Service/API_calls.js";
 
 const saveBooking = document.getElementById("saveBooking")
 const cancelBooking = document.getElementById("cancelBooking")
+const activitySelect =document.getElementById("activity")
 
+getAll("activities").then(data => {
+    console.log(data)
+    data.forEach(activity => {
+        const option = document.createElement("option")
+        option.setAttribute("value", activity.id + "")
+        option.innerText = activity.name
+
+        activitySelect.append(option)
+    })
+})
