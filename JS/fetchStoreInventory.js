9a5c226bdd4f8968534719b2a5db5fca1348582f
Dániel Szabó <kingalistairlol@gmail.com>
2022-10-07 16:05:32 +0200

    Add candy button works

--- JS/fetchStoreInventory.js
+++ JS/fetchStoreInventory.js
@@ -1,26 +1,27 @@
 const url = "http://localhost:8080/candy";
-const out = (any) => console.log(any)
 
+const addCandyBtn = document.getElementById("addCandyBtn");
 const table = document.getElementById("table")
-console.log(table)
+
+addCandyBtn.addEventListener("click", loadAddPage)
+
+function loadAddPage() {
+    window.location.href = "addCandy.html";
+}
+
 doFetchItems()
 
 async function doFetchItems() {
-    out("items")
     let item = await fetchItems();
-    out(item); //students is an array
     item.forEach(createTable)
 }
 
 function fetchItems() {
-    out("inside fetch Items")
     return fetch(url).then(response => response.json()); //returns the result of json()
 }
 
 function createTable(item) {
-
     let rowCount = table.rows.length
-    out("rowcount=" + rowCount)
     let row = table.insertRow(rowCount)
 
     let cell1 = row.insertCell(0)
@@ -28,8 +29,4 @@ function createTable(item) {
 
     let cell2 = row.insertCell(1)
     cell2.innerHTML = item.price
-
 }
-
-
-
