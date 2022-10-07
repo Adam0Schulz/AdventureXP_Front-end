d8949cd4510e3e36500685dcb8cf0526921989bb
Adam0Schulz <adam@schulz.sk>
2022-10-07 08:50:51 +0200

    booking page timeline working without being date specific and without search

--- JS/bookings.js
+++ JS/bookings.js
@@ -1,5 +1,7 @@
+import {getAll, getActivityBookings } from './Service/API_calls.js'
 
-const data = [
+
+const data01 = [
     {
         "name": "Go-kart",
         "Bookings": [
@@ -62,49 +64,87 @@ const data = [
         ]
     },
 ]
+
+const bookingBlockBackgroundColors = ['blue', 'green']
+
 const date = new Date()
 document.getElementById('date').innerText = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear()
 
-const openingHour = "8:30"
-const closingHour = "15:30"
+const numOfHours = 8
 
-const numOfHours = () => {
-    let StartHoursAndMin = openingHour.split(":")
-    StartHoursAndMin = StartHoursAndMin.map(string => parseInt(string))
+const timelineSegments = document.getElementById('timeline_segments')
 
-
+let hour = 7
+for (let i = 0; i <= numOfHours * 2; i++) {
+    const timeSegment = document.createElement('div')
+    const wholeHour = i % 2 === 0
+    const attribute = hour + ':' + (wholeHour ? '00' : '30')
+    timeSegment.setAttribute('data-before',attribute)
+    timelineSegments.append(timeSegment)
+    hour = wholeHour ? hour : hour + 1
 }
 
-console.log(numOfHours())
+//getAll('activities').then(data => console.log(data))
+//getActivityBookings(1).then(data => console.log(data))
+
+getAll('activities').then(activityData => {
+
+    activityData.forEach(activity => {
+        let activityRow = document.createElement('div')
+        activityRow.className = "activity_row"
+        let activityColumn = document.createElement('div')
+        activityColumn.className = "activity_column"
+        let activityHeading = document.createElement('h3')
+
+        activityHeading.innerText = activity.name
+
+        activityRow.appendChild(activityColumn).append(activityHeading)
+
+        let rowTimeline = document.createElement('div')
+        rowTimeline.className = "row-timeline"
+        rowTimeline.style.minWidth = timelineSegments.offsetWidth + 'px'
+        rowTimeline.setAttribute('width', timelineSegments.offsetWidth + '')
+        getActivityBookings(activity.id).then(bookingData => {
+            bookingData.forEach(booking => {
+                let bookingBlock = document.createElement('div')
+                bookingBlock.className = "booking_block"
 
 
-data.forEach(activity => {
-    let activityRow = document.createElement('div')
-    activityRow.className = "activity_row"
-    let activityColumn = document.createElement('div')
-    activityColumn.className = "activity_column"
-    let activityHeading = document.createElement('h3')
+                const oneTimeSegment = (100 / (numOfHours * 2))
 
-    activityHeading.innerText = activity.name
+                const startHour = booking.startTime.split(':', 3)[0]
+                const startMinute = booking.startTime.split( ':', 3)[1]
+                const wholeStartHour = startMinute === "00"
 
-    activityRow.appendChild(activityColumn).append(activityHeading)
+                const endHour = booking.endTime.split(':', 3)[0]
+                const endMinute = booking.startTime.split(':', 3)[1]
+                const wholeEndHour = startMinute === "00"
 
-    let rowTimeline = document.createElement('div')
-    rowTimeline.className = "row-timeline"
+                bookingBlock.style.left = ((oneTimeSegment * 2) * (startHour - 7)) + (wholeStartHour ? 0 : oneTimeSegment) + '%'
+                bookingBlock.style.right = 100 - (((oneTimeSegment * 2) * (endHour - 7)) + (wholeEndHour ? 0 : oneTimeSegment)) + '%'
 
-    activity.Bookings.forEach(booking => {
-        let bookingBlock = document.createElement('div')
-        bookingBlock.className = "booking_block"
-        bookingBlock.innerText = booking.id + "/" + booking.customer.firstName + " " + booking.customer.lastName
+                let bookingHeading = document.createElement('h4')
+                bookingHeading.innerText = "#" + booking.id + " - " + booking.customer.firstname + " " + booking.customer.lastname
 
-        rowTimeline.append(bookingBlock)
+                let bookingBody = document.createElement('p')
+                bookingBody.innerHTML= booking.startTime + " - " + booking.endTime + "<br />" + "No. of Part.: " +  booking.numberOfParticipants
+
+                bookingBlock.append(bookingHeading)
+                bookingBlock.append(bookingBody)
+
+                bookingBlock.style.backgroundColor = bookingBlockBackgroundColors[Math.floor(Math.random() * bookingBlockBackgroundColors.length)]
+
+                rowTimeline.append(bookingBlock)
+            })
+        })
+
+        activityRow.append(rowTimeline)
+        $('.calendar_body').append(activityRow)
+
+
     })
-
-    activityRow.append(rowTimeline)
-    $('.calendar_body').append(activityRow)
-
-
 })
+
 
 
 
