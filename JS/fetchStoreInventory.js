9251afe6cd3d9bb4207521cce71c90f45e380337
Mubíbic <90600712+mubibic@users.noreply.github.com>
2022-10-10 14:20:28 +0200

    fixed some things

--- JS/fetchStoreInventory.js
+++ JS/fetchStoreInventory.js
@@ -13,13 +13,19 @@ doFetchItems()
 
 function shopItems(evt, items) {
     // Declare all variables
-    let i, tabContent;
+    var i, tabContent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
     tabContent = document.getElementsByClassName("tabContent");
     for (i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
     }
+
+   /* // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }*/
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(items).style.display = "block";
