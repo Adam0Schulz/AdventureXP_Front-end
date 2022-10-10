bb139f3ecabb91031181815bffe63043556d0fed
Mubíbic <90600712+mubibic@users.noreply.github.com>
2022-10-10 13:34:34 +0200

    "finished" the adventure store

--- JS/fetchStoreInventory.js
+++ JS/fetchStoreInventory.js
@@ -11,7 +11,7 @@ function loadAddPage() {
 
 doFetchItems()
 
-function openCity(evt, cityName) {
+function openCity(evt, items) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
@@ -28,7 +28,7 @@ function openCity(evt, cityName) {
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(items).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
